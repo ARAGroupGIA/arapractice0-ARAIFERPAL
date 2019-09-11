8c3ad25d42ee701f2a456fda972f8be176fd4231
ARAIFERPAL <IFERPAL@etsid.upv.es>
2019-09-11 14:26:03 +0200

    Summing numbers Vertion 1

---
+++ Exercise1/summming numbers.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+int a;
+int b;
+int Result;
+printf("Input one number: ");
+scanf("%d",&a);
+printf("Input another number: ");
+scanf("%d",&b);
+Result=a+b;
+printf("The result is %d",Result);
+}
